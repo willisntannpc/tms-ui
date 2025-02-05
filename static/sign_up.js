4576cb300c5eb3634b95b046db0bfe9a7d81bcfe
willisntannpc <wn.mugoto@gmail.com>
2025-02-06 01:23:21 +0300

    new changes

--- static/sign_up.js
+++
@@ -1,5 +0,0 @@
-// JavaScript to toggle between Login and Signup forms
-function toggleForm(showId, hideId) {
-    document.getElementById(showId).style.display = 'block';
-    document.getElementById(hideId).style.display = 'none';
-}
